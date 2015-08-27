1db4035e11a83928cdac3fb9fbc34dcb53a8d5d1
whyzack <whyzack@gmail.com>
2015-08-27 14:12:11 -0300

    Update change_status.sh

--- change_status.sh
+++ change_status.sh
@@ -4,24 +4,24 @@
 >execoes
 >search
 
-/opt/zimbra/bin/zmprov gadl | cut -d "@" -f1 >>execoes
-echo "galsync" >>execoes
-echo "root" >>execoes
-echo "spam" >>execoes
-echo "admin" >>execoes
-echo "postmaster" >>execoes
-echo "virus-quarantine" >>execoes
+/opt/zimbra/bin/zmprov gadl | cut -d “@” -f1 >>execoes
+echo “galsync” >>execoes
+echo “root” >>execoes
+echo “spam” >>execoes
+echo “admin” >>execoes
+echo “postmaster” >>execoes
+echo “virus-quarantine” >>execoes
 
-ldapsearch -D "cn=config" -w "jvwPgTi0D" -h 10.1.13.90 -b "dc=copasa,dc=com,dc=br" -s sub dn | grep "uid" >>search
+ldapsearch -D “cn=config” -w “SENHA_LDAP” -h IP_SERVIDOR -b “dc=DOMINIO,dc=com,dc=br” -s sub dn | grep “uid” >>search
 
 cat search | grep -v -f execoes >>lista
 
 while read line
 do
 echo $line>>modify
-echo "changetype:modify">>modify
-echo "replace: zimbraAccountStatus">>modify
-echo "zimbraAccountStatus: active">>modify
-echo "">>modify
+echo “changetype:modify”>>modify
+echo “replace: zimbraAccountStatus”>>modify
+echo “zimbraAccountStatus: locked”>>modify
+echo “”>>modify
 done <lista
-ldapmodify -D "cn=config" -w "jvwPgTi0D" -h 10.1.13.90 -f modify
+ldapmodify -D “cn=config” -w “SENHA_LDAP” -h IP_SERVIDOR -f modify
